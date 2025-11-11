93e889ce60ac54edee26810f8e2f84019541fbd2
ole-kristian <olemofo@gmail.com>
2025-11-11 19:10:45 +0100

    added pathFindObject func

--- src/ConsoleClientApp/Program.cs
+++ src/ConsoleClientApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using MapGeneratorCs;
+using MapGeneratorCs.PathFinding;
+using MapGeneratorCs.Types;
 
 internal class Program
 {
@@ -7,5 +9,21 @@ internal class Program
         var map = new MapConstructor();
         map.GenerateMap();
         map.SaveMapAsImage();
+
+        var pathGenerator = new PathGenerator(map.NodeContainer, includePrintLog: true);
+
+        // use static helper instead of instance method
+        var closest = pathGenerator.FindClosestObjectNodesOfType(
+            new Vect2D(10, 10),
+            map.NodeContainer.NodesObjects,
+            maxSearchDistance: 50,
+            maxObjectCount: 5
+        );
+
+        Console.WriteLine($"Found {closest.Count} matching object nodes within distance 50.");
+        foreach (var pos in closest)
+        {
+            Console.WriteLine($" - Object at position {pos}");
+        }
     }
 }
