bd7069e518ff2ab6137f242298be670083cef629
ole-kristian <olemofo@gmail.com>
2025-11-15 03:48:11 +0100

    program fix, WORKING! going to bed

--- src/ConsoleClientApp/Program.cs
+++ src/ConsoleClientApp/Program.cs
@@ -5,12 +5,22 @@ using MapGeneratorCs.PathFinding.Dijkstra.Utils;
 using MapGeneratorCs.PathFinding.Utils;
 using MapGeneratorCs.PathFinding.Dijkstra;
 using MapGeneratorCs.PathFinding.Image;
+using MapGeneratorCs.Generator.Types;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        var map = new MapConstructor();
+        MapConstructor map;
+
+        if (args.Length > 0 && int.TryParse(args[0], out int len))
+            map = new MapConstructor(
+                overwriteExisting: true,
+                spawnWeights: null,
+                mapConfig: new MapConfig() { Length = len }
+            );
+        else
+            map = new MapConstructor();
         map.GenerateMap();
         map.GeneratePathNodes();
         map.SaveMapAsImage();
